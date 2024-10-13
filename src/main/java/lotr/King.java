96951a09b20fed36758d7cbbbb92e6ee08558968
Ruslan689 <dovhai.pn@ucu.edu.ua>
2024-10-13 17:25:11 +0300

    add something

--- src/main/java/lotr/King.java
+++ src/main/java/lotr/King.java
@@ -7,8 +7,9 @@ public class King extends Character{
         this.power = (int) Math.random() * 11 + 5;
     }
 
-    public void kick(Character c) { 
-        c.hp -= power;
+    public void kick(Character c) {
+        int damage = (int) (Math.random() * this.power);
+        c.setHp(c.getHp() - damage);
         // decrease number of hp of the enemy by random 
         // number which will be in the range of his power 
       }
